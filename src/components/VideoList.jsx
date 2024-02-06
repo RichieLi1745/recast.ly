f1b86082e7a2e0735b4031859a09657e4016c5d2
Richard Li <richardli1745@gmail.com>
2024-02-05 19:45:55 -0800

    make changes to recastly

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,16 @@
-var VideoList = () => (
+import VideoListEntry from "./VideoListEntry.js";
+
+var VideoList = (props) => (
+  //use map to refactor video list to take in one movie at a time
   <div className="video-list">
+    {props.videos.map((video, index)=> (
+      <VideoListEntry key={index} video={video}/>
+    ))}
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
